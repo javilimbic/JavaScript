530eeeeaf98f4d88bd1817d036833d78b901a1f0
javilimbic <javi@limbicweb.com>
2016-02-25 09:04:04 +0100

    Añadido js para cambiar alguna frase si el tema no lo hiciera

---
+++ cambio idioma.js
@@ -0,0 +1,7 @@
+$(function){
+	if($("html").attr("lang")=="ca"){
+		$(".eg-caso-exito-element-2-a").children("a").html("VEURE MÉS");
+	}else{
+		$(".eg-caso-exito-element-2-a").children("a").html("VER MÁS");		
+	}
+}
